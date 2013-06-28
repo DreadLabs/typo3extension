eb3ee0133f7812bf4518f98fa3e040be0ec072e3
Thomas Juhnke <tommy@van-tomas.de>
2013-06-28 21:32:00 +0200

    [INIT] initial commit

---
+++ README.rst
@@ -0,0 +1,20 @@
+=====================
+typo3extensionproject
+=====================
+
+A TYPO3 extension kickstart package for usage with composer dependency manager.
+
+How to use
+----------
+
+1. Download the Composer Executable
+
+..code:: shell
+	$ curl -sS https://getcomposer.org/installer | php
+
+2. Init your new TYPO3 extension project
+
+..code:: shell
+	$ php composer.phar create-project dreadlabs/typo3extensionproject
+
+3. Start coding!
---
+++ src/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "@@@EXTENSION_KEY@@@".
+ *
+ * Auto generated @@@GENERATION_DATETIMESTAMP@@@17-01-2013 21:01
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '@@@EXTENSION_TITLE@@@',
+	'description' => '@@@EXTENSION_DESCRIPTION@@@',
+	'category' => '@@@EXTENSION_CATEGORY@@@',
+	'author' => '@@@EXTENSION_AUTHOR@@@',
+	'author_email' => '@@@EXTENSION_AUTHOR_EMAIL@@@',
+	'shy' => '',
+	'dependencies' => '@@@EXTENSION_DEPENDENCIES@@@',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '@@@EXTENSION_VERSION@@@',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.0.4-6.0.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
